29b3ed1821cfaab8af17c2d9e329f644b8d7cabf
crisOspina <crisospina523@gmail.com>
2024-01-15 09:09:20 -0500

    feat(app-constants): add routes list

---
+++ app/_constants/routes.ts
@@ -0,0 +1,11 @@
+type Routes = {
+  signin: string
+  dashboard: string
+  race: string
+}
+
+export const route: Routes = {
+  signin: '/ingresar',
+  dashboard: '/universo',
+  race: '/universo/raza'
+}
