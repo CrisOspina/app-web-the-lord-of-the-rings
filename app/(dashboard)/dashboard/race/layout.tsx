3eb1edea53fb88f092b775915da5381a5ee6f8b6
crisOspina <crisospina523@gmail.com>
2024-02-01 13:28:45 -0500

    feat(app): remove validation routes from layouts

--- app/(dashboard)/dashboard/race/layout.tsx
+++ app/(dashboard)/dashboard/race/layout.tsx
@@ -1,20 +1,10 @@
 import type { ReactNode } from 'react'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-import { route } from '@/app/_constants/routes'
 
 import BgForRace from './_components/bg-for-race'
 import Header from '@/app/_components/header'
 import Navbar from '@/app/_components/navbar'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const cookie = cookies()
-  const value = cookie.get('auth')?.value
-  const hasKey = cookie.has('auth')
-
-  if (!hasKey && !value) redirect(route.signin)
-
   return (
     <BgForRace>
       <Header />
